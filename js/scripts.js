f2b3ef20daced77f3285508fe794c7471b1dad9c
AlFexx <dukeofthemyst@mail.ru>
2022-11-16 14:54:44 +0300

    16-11-11

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,6 @@ const menuBtn = document.querySelector('.cat-main')
 const menu = document.querySelector('.navigation-subitem-list')
 menuBtn.addEventListener('click', (e) => {
   e.preventDefault();
+  menuBtn.classList.toggle('is-active');
   menu.classList.toggle('is-active')
 })
